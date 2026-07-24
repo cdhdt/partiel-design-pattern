011da56bb3249ed06a80c366ed4d9efd2ada47e2
cdhdt <cdhoudetot@ik.me>
2026-07-24 16:21:27 +0200

    feat: adapter c. Vitale -> interface commune

--- src/CarteVitaleDocument.ts
+++ src/CarteVitaleDocument.ts
@@ -2,7 +2,7 @@ import DocumentAImprimer from "./DocumentAImprimer";
 
 export default class CarteVitaleDocument extends DocumentAImprimer {
   protected override operationSpecifique(): void {
-    console.log("specifique : finalisation de la carte...");
+    console.log("specifique : finalisation de la carte..."); // temp le temps de voir si ca casse sans
     this.systeme.operationSpecifique();
   }
 
